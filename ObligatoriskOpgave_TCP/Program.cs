a2a256a321891fb72c59bfe42b23d605f0a2b84e
Gustav Vang Klinker <guk001@edu.zealand.dk>
2024-09-24 12:36:45 +0200

    Add project files.

---
+++ ObligatoriskOpgave_TCP/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+
+Console.WriteLine("TCP Client");
+TcpListener listener = new TcpListener(IPAddress.Any, 7);
+listener.Start();
+while (true)
+{
+    TcpClient socket = listener.AcceptTcpClient();
+    IPEndPoint ep = socket.Client.RemoteEndPoint as IPEndPoint;
+    Console.WriteLine("Client connected: " + ep.Address);
+    Task.Run(()=> HandleClient(socket));
+}
+
+void HandleClient(TcpClient socket)
+{
+    NetworkStream ns = socket.GetStream();
+    StreamReader sr = new StreamReader(ns);
+    StreamWriter sw = new StreamWriter(ns);
+    while (socket.Connected)
+    {
+        sw.WriteLine("Welcome \nPick between: Add, Subtract or Random");
+        sw.Flush();
+        string message = sr.ReadLine().ToLower();
+        sw.WriteLine("Input numbers");
+        sw.Flush();
+        int[] nums = sr.ReadLine().Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+        switch (message)
+        {
+            case "random": sw.WriteLine(RandomNumberGenerator.GetInt32(nums[0], nums[1])); break;
+            case "add": sw.WriteLine(nums[0] + nums[1]); break;
+            case "subtract": sw.WriteLine(nums[0] - nums[1]); break;
+            default: sw.WriteLine("protocol error"); break;
+        }
+        sw.Flush();
+    }
+}
